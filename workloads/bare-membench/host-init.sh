4c520ad69201cc72df6fad574739719320f590f8
Björn Gottschall <github.mail@bgottschall.de>
2022-06-10 12:49:52 +0200

    * cleanup

--- workloads/bare-membench/host-init.sh
+++ workloads/bare-membench/host-init.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+
 make -C bwtest clean
 make -C bwtest bare-bwtest
+cp -a bwtest/bare-bwtest ./
 
 make -C pointers clean
 make -C pointers bare-pointers
+cp -a pointers/bare-pointers ./
+
+[ ! -e ../../images/bare-membench-pointers.img ] && ln -rs dummy.img ../../images/bare-membench-pointers.img
+[ ! -e ../../images/bare-membench-bwtest.img ] && ln -rs dummy.img ../../images/bare-membench-bwtest.img
+[ ! -e ../../images/bare-membench-pointers-bin ] && ln -rs bare-pointers ../../images/bare-membench-pointers-bin
+[ ! -e ../../images/bare-membench-bwtest-bin ] && ln -rs bare-bwtest ../../images/bare-membench-bwtest-bin
+
+exit 0
