74ffbf9316c1cd134099c0206ac01260a3e758b8
Björn Gottschall <github.mail@bgottschall.de>
2022-06-22 12:25:22 +0200

    * lowered strided bandwidth test size

--- workloads/membench/host-init.sh
+++ workloads/membench/host-init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 make -C overlay/opt/bwtest/ clean
-make -C overlay/opt/bwtest/ bwtest bwstride
+make -C overlay/opt/bwtest/ bwtest 
+make -C overlay/opt/bwtest/ DEFINES=-DSTREAM_SIZE=$((512 * 1024 * 1024)) bwstride
 
 make -C overlay/opt/pointers/ clean
 make -C overlay/opt/pointers/ pointers
